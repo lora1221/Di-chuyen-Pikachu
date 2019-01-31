2baf42555a5adcef65da7bfb299ba594aa876a17
lora1221 <lori.tran1221@gmail.com>
2019-01-30 23:43:36 -0600

    1st

--- script.js
+++ script.js
@@ -14,26 +14,61 @@ function Hero(image, top, left, size, speed){
         return '<img width="'+ this.size + '"' +
             ' height="'+ this.size + '"' +
             ' src="' + this.image +'"' +
-            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-    }
-
-    this.moveCrazy = function(){
-        this.top = Math.random()*(window.innerHeight - this.size) ;
-        this.left = Math.random()*(window.innerWidth - this.size);
-        this.count ++;
+            ' style="top: '+this.left+ 'px; left:'+this.left+'px;position:absolute;" />';
     };
 
+    // this.moveCrazy = function(){
+    //     this.top = Math.random()*(window.innerHeight - this.size) ;
+    //     this.left = Math.random()*(window.innerWidth - this.size);
+    //     this.count ++;
+    // };
+
+    this.moveRight = function(){
+        this.left += 30;
+    };
+
+    this.moveLeft = function(){
+        this.left -= 30;
+    };
+    this.moveDown = function(){
+        this.top += 30;
+    };
+    this.moveUp = function(){
+        this.top -=30;
+    };
 
 }
 
 var hero = new Hero('doremon.png', 20, 50, 200, 50);
-
+var touchRight = false;
+var touchBottom = false;
+var touchLeft = false;
 function start(){
-    if (hero.count < 50) {
-        hero.moveCrazy();
-    }
+   // if (!touchRight) {
+        if (hero.left < window.innerWidth - hero.size) {
+            // alert(hero.left);
+            // alert(hero.top);
+            hero.moveRight();
+        }
+    // } else {
+    //     if (hero.top < 500 - hero.size) {
+    //         hero.moveDown();
+    //     } else {
+    //         touchBottom = true;
+    //     }
+
+
+    // if (touchBottom) {
+    //     if (hero.left > hero.size) {
+    //         hero.moveLeft();
+    //     } else {
+    //         touchLeft = true;
+    //     }
+    // }
+
+
     document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 200)
+    setTimeout(start, 100)
 }
 
 start();
